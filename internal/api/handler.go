4201245df93811550c50b45b637a212c7ce3a236
Victor Vityuk <vicv@1-vc.ru>
2025-01-10 21:12:05 +0300

    реализовал недостоющие методы

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -50,6 +50,14 @@ func (h *handler) AddIncidents(ctx context.Context, req oas.AddIncidentsReq) (oa
 }
 
 func (h *handler) DeleteIncident(ctx context.Context, params oas.DeleteIncidentParams) error {
+	id, err := strconv.Atoi(params.IncidentId)
+	if err != nil {
+		return fmt.Errorf("invalid request type: %T", params)
+	}
+	err = h.incidents.Delete(ctx, id)
+	if err != nil {
+		return fmt.Errorf("failed to get incident: %w", err)
+	}
 	return nil
 }
 
@@ -75,9 +83,51 @@ func (h *handler) GetIncidentById(
 }
 
 func (h *handler) GetIncidents(ctx context.Context) (oas.GetIncidentsRes, error) {
-	return nil, nil
+
+	incidents, err := h.incidents.List(ctx, 50, 0)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to add incident: %w", err)
+	}
+
+	req := make([]oas.Incident, 0, 50)
+	for _, val := range incidents {
+		req = append(req, oas.Incident{
+			ID:     fmt.Sprintf("%d", val.ID),
+			Region: val.Region,
+			Fio:    val.FIO,
+			Status: val.Status,
+			Date:   val.Date,
+		})
+
+	}
+
+	return &oas.IncidentsResponse{Data: req}, nil
 }
 
 func (h *handler) UpdateIncidents(ctx context.Context, req oas.UpdateIncidentsReq) (oas.UpdateIncidentsRes, error) {
-	return nil, nil
+	request, ok := req.(*oas.UpdateIncidentsApplicationJSON)
+	if !ok {
+		return &oas.UpdateIncidentsBadRequest{}, fmt.Errorf("invalid request type: %T", req)
+	}
+
+	incident := &models.Incident{
+		IsTraining: false, //! Нет поля в oas
+		Region:     request.Region,
+		FIO:        request.Fio,
+		Status:     request.Status,
+		Date:       request.Date,
+	}
+
+	if err := h.incidents.Update(ctx, incident); err != nil {
+		return nil, fmt.Errorf("failed to add incident: %w", err)
+	}
+
+	return &oas.Incident{
+		ID:     fmt.Sprintf("%d", incident.ID),
+		Region: incident.Region,
+		Fio:    incident.FIO,
+		Status: incident.Status,
+		Date:   incident.Date,
+	}, nil //? Не понятно что вернуть
 }
